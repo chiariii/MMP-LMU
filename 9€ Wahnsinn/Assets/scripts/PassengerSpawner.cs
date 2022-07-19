c2b3e089277a1ac5323f16e8622ef8f50124f503
thomsenj <jonas.thomsen@campus.lmu.de>
2022-07-19 17:30:37 +0200

    Passenger Spawner + Working Animation
    Passengers are apparently spawning outside or under the map or are not visible.
    
    Co-Authored-By: maxK208 <108215934+maxK208@users.noreply.github.com>

--- 9€ Wahnsinn/Assets/scripts/PassengerSpawner.cs
+++ 9€ Wahnsinn/Assets/scripts/PassengerSpawner.cs
@@ -4,43 +4,41 @@ using UnityEngine;
 
 public class PassengerSpawner : MonoBehaviour
 {
-    /*
+[SerializeField]
+    private GameObject PassengerPrefab1;
+
+    //[SerializeField]
+    //private GameObject pigeonPrefab2;
+
+
     [SerializeField]
-    private GameObject[] asteroidPrefabs;
-    [SerializeField]
-    private float respawnTime = 1.0f;
+    private float respawnTime  = 5f;
 
     private Vector2 screenBounds;
 
-    // Start is called before the first frame update
+   
+
     void Start()
     {
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-        StartCoroutine(AsteroidWave());
-       // var timer = new Timer();
-
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        //StartCoroutine(PassengerSpawn());
     }
-    
-    private void SpawnAsteroid()
+
+     void AddPassenger()
     {
-        int randomIndex = Random.Range(0, asteroidPrefabs.Length);
-        GameObject asteroidPrefab = asteroidPrefabs[randomIndex];
-        if (!asteroidPrefab)
-        {
-            return; // because array fields can be set to null
-        }
-        GameObject asteroidInstance = Instantiate(asteroidPrefab) as GameObject;
+        GameObject[] passengers = {PassengerPrefab1};
+        int index = Random.Range(0, passengers.Length);
+
+        GameObject passengersInstance = Instantiate(passengers[index]) as GameObject;
         float spawnX = Random.Range(-screenBounds.x, screenBounds.y);
-        asteroidInstance.transform.position = new Vector2(spawnX, screenBounds.y * 2);
+        passengersInstance.transform.position = new Vector2(spawnX, screenBounds.y * 2);
     }
 
-    private IEnumerator AsteroidWave()
+    private IEnumerator PassengerSpawn()
     {
-        while (true)
-        {
+        while(true){
             yield return new WaitForSeconds(respawnTime);
-            SpawnAsteroid();
+            AddPassenger();
         }
     }
-*/
 }
--- 9€ Wahnsinn/Assets/scripts/Passenger_01_Movement.cs
+++ 9€ Wahnsinn/Assets/scripts/Passenger_01_Movement.cs
@@ -4,24 +4,44 @@ using UnityEngine;
 
 public class Passenger_01_Movement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float speed;
 
-    //public Rigidbody2D rb;
+    public Rigidbody2D rb;
     public Animator animator;
 
     Vector2 movement;
+    private Vector2 screenBounds;
 
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        
+        rb.velocity = new Vector2(0, speed);
+
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    }
     // Update is called once per frame
     void Update()
     {
-        /*
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        
+        //movement.x = Input.GetAxisRaw("Horizontal");
+        //movement.y = Input.GetAxisRaw("Vertical");
         //transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
-        animator.setFloat("Horizontal", movement.x);
-        animator.setFloat("Vertical", movement.y);
-        animator.setFloat("Speed", movement.sqrMagnitude);
-        */
+       
+        movement.x = 0;
+        movement.y = -transform.position.y; 
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        //if (movement.y > screenBounds.y * 2)
+        Debug.Log(movement.sqrMagnitude);
+        if (movement.sqrMagnitude <= 0.001f)
+        {
+           Debug.Log(movement.sqrMagnitude);
+           Destroy(this.gameObject);
+        }
+        
     }
 
     void FixedUpdate()
